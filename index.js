616c7678564f1157dd2672033c25ce56a801f5ec
github username <github email address>
2021-02-02 14:48:20 -0500

    commit tree ab0747fcb3878a8a8c90b8116cd27896e18183b3

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+
+function upperCaseCustomerName() {
+    console.log(customerName.toUpperCase())
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    console.log(bestCustomer)
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+    console.log(bestCustomer)
+}
+
+const leastFavoriteCustomer = 'victor'
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'mel'
+  }
